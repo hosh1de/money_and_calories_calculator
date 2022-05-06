7bcbf71fd026d2a3848168a107a699ca16ce0a7e
Iaroslav Bratovnikov <hoshide@vk.com>
2022-05-06 23:33:02 +0300

    Updated Record class Added CaloriesCalculator class Added CashCalculator class

--- Program.py
+++ Program.py
@@ -5,13 +5,12 @@ class Record:
     def __init__(self, amount, comment, date=dt.datetime.now().date()):
         self.amount = amount
         self.comment = comment
-        try:
-            date = str(date.strftime('%d.%m.%Y'))
-        except:
-            pass
         date_format = '%d.%m.%Y'
-        date = dt.datetime.strptime(date, date_format).date()
-        self.date = date
+        try:
+            date = dt.datetime.strptime(date, date_format).date()
+            self.date = date
+        except:
+            self.date = date
 
 
 class Calculator:
@@ -47,15 +46,54 @@ class Calculator:
         return total
 
 
-# class CaloriesCalculator(Calculator):
-#     def get_calories_remained():
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):
+        total_today = self.get_today_stats()
+        if total_today < self.limit:
+            return (f'Сегодня можно съесть что-нибудь ещё, но с общей '
+                    f'калорийностью не более {self.limit - total_today} кКал')
+        else:
+            return 'Хватит есть!'
 
 
+class CashCalculator(Calculator):
+    USD_RATE = 67.38
+    EURO_RATE = 71.10
 
-calculator = Calculator(1000)
-calculator.add_record(Record(amount=145, comment="кофе"))
-calculator.add_record(Record(amount=300, comment="Серёге за обед"))
-calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
-r4 = calculator.get_week_stats()
-r5 = calculator.get_today_stats()
-print()
+    def get_today_cash_remained(self, currency):
+        total_today = self.get_today_stats()
+
+        if currency == 'rub':
+            if total_today == self.limit:
+                return 'Денег нет, держись'
+            elif total_today < self.limit:
+                return f'На сегодня осталось {self.limit - total_today} руб'
+            else:
+                return (f'Денег нет, держись: '
+                        f'твой долг - {total_today - self.limit} руб')
+        elif currency == 'usd':
+            total_today /= self.USD_RATE
+            usd_limit = self.limit / self.USD_RATE
+            
+            if total_today == usd_limit:
+                return 'Денег нет, держись'
+            elif total_today < usd_limit:
+                result_rnd = round(usd_limit - total_today, 2)
+                return f'На сегодня осталось {result_rnd} USD'
+            else:
+                result_rnd = round(total_today - usd_limit, 2)
+                return (f'Денег нет, держись: '
+                        f'твой долг - {result_rnd} USD')
+        elif currency == 'eur':
+            total_today /= self.EURO_RATE
+            euro_limit = self.limit / self.EURO_RATE
+            
+            if total_today == euro_limit:
+                return 'Денег нет, держись'
+            elif total_today < euro_limit:
+                result_rnd = round(euro_limit - total_today, 2)
+                return f'На сегодня осталось {result_rnd} USD'
+            else:
+                result_rnd = round(total_today - euro_limit, 2)
+                return (f'Денег нет, держись: '
+                        f'твой долг - {result_rnd} USD')
